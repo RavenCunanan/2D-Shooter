1db3e8e9c6653ab95c08847470dab14177395009
Raven Cunanan <63638637+RavenCunanan@users.noreply.github.com>
2025-03-07 09:32:05 -0800

    Update main.py
    Added meteor rotation

--- main.py
+++ main.py
@@ -56,17 +56,23 @@ class Laser(pygame.sprite.Sprite):
 class Meteor(pygame.sprite.Sprite):
     def __init__(self, surf, pos, groups):
         super().__init__(groups)
+        self.orignal_surf = surf
         self.image = surf
         self.rect = self.image.get_frect(center = pos)
         self.start_time = pygame.time.get_ticks()
         self.lifetime = 3000
         self.direction = pygame.Vector2(random.uniform(-0.5, 0.5),1)
         self.speed = random.randint(400,500)
+        self.rotation_speed = random.randint(40,80)
+        self.rotation = 0
     
     def update(self,dt):
         self.rect.center += self.direction * self.speed * dt
         if pygame.time.get_ticks() - self.start_time >= self.lifetime:
             self.kill()
+        self.rotation += self.rotation_speed * dt
+        self.image = pygame.transform.rotozoom(self.orignal_surf, self.rotation, 1)
+        self.rect = self.image.get_frect(center = self.rect.center)
 
 def collisions():
     global running
